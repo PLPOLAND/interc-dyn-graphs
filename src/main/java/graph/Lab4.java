7715735d5539c2d33122be06cff21e66b3862fd6
PLPOLAND <marekpaldyna@wp.pl>
2021-12-02 22:35:41 +0100

    lab4 ex.1 & ex.2(problem with mutlithreads)

--- src/main/java/graph/Lab4.java
+++ src/main/java/graph/Lab4.java
@@ -1,5 +1,291 @@
 package graph;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+import javax.sql.rowset.serial.SerialArray;
+
+import org.graphstream.graph.implementations.SingleGraph;
+
+import scala.annotation.elidable;
+
+import org.graphstream.graph.Node;
+import org.graphstream.algorithm.Toolkit;
+import org.graphstream.graph.Edge;
+import org.graphstream.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
 public class Lab4 {
+    SingleGraph myGraph;
+    Random random;
+    static int boxSize = 10;
+    static int sleepTime = 10;
+    public Lab4(String[] args){
+
+        // random = new Random(1);
+        random = new Random(System.currentTimeMillis());
+
+        // ex1(100,0.1);
+        ex2(15,1,20,1);
+        
+    }
+
+    private void ex2(int n, int threads, int _boxSize, int _sleepTime) {
+        boxSize = _boxSize;
+        sleepTime = _sleepTime;
+        Ex2Entity entity[] = new  Ex2Entity[threads];
+
+        
+
+        myGraph = Tools.grid(n, false, true);
+        myGraph.display(false);
+        for (Node node : myGraph.getEachNode()) {
+            int r = random.nextInt(254);
+            int g = random.nextInt(254);
+            int b = random.nextInt(254);
+            node.addAttribute("ui.style", "fill-color: rgb("+r+","+g+","+b+");shape: box;size:"+boxSize+"px;");
+            node.addAttribute("alive", 0);
+            node.addAttribute("color", ""+r+","+g+","+b+"");
+            for (Edge edge : node.getEachEdge()) {
+                edge.addAttribute("ui.style", "fill-color: rgb(255,255,255);");
+            }
+        }
+
+        // Tools.pause(1000);
+        for (int i = 0; i < threads; i++)
+            entity[i] = new Ex2Entity(myGraph);
+        for (Ex2Entity ex2Entity : entity) {
+            ex2Entity.start();
+        }
+        // while (true) {
+        //     for (Ex2Entity ex2Entity : entity) {
+        //         ex2Entity.makeNext();
+        //     }
+        // }
+
+    }
+
+    //GAME_OF_LIFE
+    public void ex1(int n ,double alive) {
+        boxSize = 5;
+
+        myGraph = Tools.grid(n,true,false);
+        myGraph.display(false);
+        for (Node node : myGraph.getEachNode()) {
+            node.addAttribute("ui.style", "fill-color: rgb(255,255,255);shape: box;size:"+boxSize+"px; stroke-mode: plain; stroke-width:1px; stroke-color:#000000;");
+            node.addAttribute("alive", 0);
+            for (Edge edge : node.getEachEdge()) {
+                edge.addAttribute("ui.style", "fill-color: rgb(255,255,255);");
+            }
+        }
+
+        initState(myGraph, alive);
+        Tools.hitakey("hit to start GOL");
+        while (true) {
+            Tools.pause(100);
+            for (Node node : myGraph.getEachNode()) {//for each cell(node)
+                int howManyAliveNeighbor = 0;
+                Iterator<Node> iter = node.getNeighborNodeIterator();
+                
+                while (iter.hasNext()) {//count alive neighbor
+                    Node friend = iter.next();
+                    if ((int)friend.getAttribute("alive")==1) {
+                     howManyAliveNeighbor++;   
+                    }
+                }
+
+                if((int) node.getAttribute("alive") == 1){//GL logic
+                    if(howManyAliveNeighbor == 2 || howManyAliveNeighbor == 3){
+                        ;
+                    }
+                    else{
+                        markDead(node);
+                    }
+                }
+                else{
+                    if(howManyAliveNeighbor == 3){
+                        markAlive(node);
+                    }
+                }
+            }
+        }
+
+    }
+
+    private void initState(SingleGraph graph, double proc){
+        int howManyNodes= graph.getNodeCount();
+        int toMakeAlive = (int)(howManyNodes * proc);
+
+        while (toMakeAlive-- !=0) {
+            Node n = Toolkit.randomNode(myGraph,random);
+            if((int)n.getAttribute("alive") == 0){
+                markAlive(n);
+            }
+            else{
+                toMakeAlive++;
+            }
+        }
+
+    }
+
+    private void markAlive(Node node){
+        node.removeAttribute("ui.style");
+        node.addAttribute("ui.style",
+                "fill-color: #000000;shape: box;size:"+boxSize+"px; stroke-mode: plain; stroke-width:1px; stroke-color:#000000;");
+        node.changeAttribute("alive", 1);
+    }
+    private void markDead(Node node){
+        node.removeAttribute("ui.style");
+        node.addAttribute("ui.style",
+                "fill-color: #FFFFFF;shape: box;size:"+boxSize+"px; stroke-mode: plain; stroke-width:1px; stroke-color:#000000;");
+        node.changeAttribute("alive", 0);
+    }
+
+    public static void main(String[] args) {
+        System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+        new Lab4(args);
+    }
+}
+
+
+/**
+ * ex2
+ */
+class Ex2Entity extends Thread{
+    boolean kill = false;
+
+    boolean makeNext = false;
+
+    SingleGraph graph;
+
+    HashMap<String,Integer> colors;
     
+    ReentrantLock lock = new ReentrantLock();
+    ReentrantLock lock2 = new ReentrantLock();
+
+    String bestColor = "";
+    int bestColorVal = 0;
+
+    Ex2Entity(SingleGraph graph2){
+        super();
+        this.graph = graph2;
+        makeNext = true;
+
+    }
+
+    public void run(){
+        while (!kill) {
+            try {
+                Tools.pause(Lab4.sleepTime);
+                // System.out.println("!kill");
+                bestColor ="";
+                bestColorVal= 0;
+                // if (makeNext) {
+                    // System.out.println("makeNext: " + this.getName());
+                    colors = new HashMap<>();
+                    Node node = Toolkit.randomNode(graph);
+                    if(lock.tryLock(10, TimeUnit.SECONDS)){
+                        try {
+                            System.out.println(this.getName() + " startof :" + node.getId());
+                            Iterator<Node> neigh = node.getNeighborNodeIterator();
+                            while (neigh.hasNext()) {// compute number of colors
+                                Node currNode = neigh.next();
+                                String color = currNode.getAttribute("color");
+                                if (colors.containsKey(color)) {
+                                    colors.replace(color, colors.get(color) + 1);
+                                } else {
+                                    colors.put(color, 1);// add new color to map;
+                                }
+
+                            }
+
+                            colors.forEach((color, val) -> {
+                                if (val > bestColorVal) {
+                                    bestColorVal = val;
+                                    bestColor = color;
+                                }
+                            });
+                            if (bestColorVal == 1) {// if there is no most color, just rand it;
+                                Random rand = new Random(System.currentTimeMillis());
+                                bestColor = (String) colors.keySet().toArray()[rand.nextInt(colors.size())];
+                            }
+
+                            // set new color to neighbor
+                            neigh = node.getNeighborNodeIterator();
+                            while (neigh.hasNext()) {// compute number of colors
+                                if (lock.tryLock(10, TimeUnit.SECONDS)) {
+                                Node currNode = neigh.next();
+                                    try {//TODO there is the problem with NoSuchElementException and with breaking viewer of graph...
+                                        /**
+                                         * java.util.NoSuchElementException
+                                         * at java.util.LinkedList.removeFirst(LinkedList.java:270)
+                                         * at java.util.LinkedList.remove(LinkedList.java:685)
+                                         * at org.graphstream.stream.SourceBase.manageEvents(SourceBase.java:872)
+                                         * at
+                                         * org.graphstream.stream.SourceBase.sendAttributeChangedEvent(SourceBase.java:810)
+                                         * at
+                                         * org.graphstream.util.GraphListeners.sendAttributeChangedEvent(GraphListeners.java:73)
+                                         * at
+                                         * org.graphstream.graph.implementations.AbstractNode.attributeChanged(AbstractNode.java:94)
+                                         * at
+                                         * org.graphstream.graph.implementations.AbstractElement.addAttribute(AbstractElement.java:544)
+                                         * at
+                                         * org.graphstream.graph.implementations.AbstractElement.setAttribute(AbstractElement.java:560)
+                                         * at graph.Ex2Entity.run(Lab4.java:224)
+                                         */
+                                        currNode.setAttribute("color", bestColor);
+                                        currNode.setAttribute("ui.style", "fill-color: rgb(" + bestColor
+                                                + ");shape: box;size:" + Lab4.boxSize
+                                                + "px; stroke-mode: plain; stroke-width:1px; stroke-color:#000000;");
+                                    } 
+                                    // catch(NoSuchElementException e){
+                                    //     e.printStackTrace();
+                                    //     graph.display(false);
+                                    //     currNode.removeAttribute("ui.style");
+                                    //     currNode.addAttribute("ui.style", "fill-color: rgb(" + bestColor+ ");shape: box;size:" + Lab4.boxSize+ "px; stroke-mode: plain; stroke-width:1px; stroke-color:#000000;");
+                                    // }
+                                    finally{
+                                        lock.unlock();
+                                    }
+                                }
+                            }
+                            node.changeAttribute("color", bestColor);
+                            node.setAttribute("ui.style", "fill-color:rgb(" + bestColor + ");");
+
+                            makeNext = false;
+                            System.out.println(this.getName() + " end of :" + node.getId());
+                        } finally {
+                            lock.unlock();
+                        }
+                        
+                    }
+                    
+                // }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    kill = true;
+                }
+            }
+            System.out.println("End of thread: " + this.getName());
+            
+    }
+    /**
+     * allow thread to make next thing
+     */
+    public void makeNext(){
+        this.makeNext = true;
+    }
+
+    /**
+     * stopping thread
+     */
+    public void killThread(){
+        kill = true;
+    }
 }
--- src/main/java/graph/Tools.java
+++ src/main/java/graph/Tools.java
@@ -1,37 +1,189 @@
 package graph;
 
 import java.io.IOException;
+import java.util.Random;
 
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.SingleGraph;
 import org.graphstream.stream.file.FileSinkImages;
 
 public class Tools {
 
+	// =============== GENERATORS ==================
 
-	public static void screenshot(SingleGraph graph, String pathToImage) { 
-		if(graph != null) if(graph.getNodeCount() > 0) {
-			FileSinkImages fsi = new FileSinkImages( FileSinkImages.OutputType.PNG, FileSinkImages.Resolutions.SVGA);
-			fsi.setLayoutPolicy(FileSinkImages.LayoutPolicy.COMPUTED_FULLY_AT_NEW_IMAGE);
-			try {
-				fsi.writeAll(graph, pathToImage);
-			} catch (IOException e) { e.printStackTrace(); } 
+	/**
+	 * Grid generator with VonNeumann neighborhood
+	 * --> every vertex non located on the border will have exactly
+	 * 4 neighbors (north, south, east and west)
+	 * The algorithm consists first in creating the vertices and
+	 * then to add edges between them.
+	 * The position of the vertices will be fixed.
+	 * 
+	 * @param n
+	 */
+	public static SingleGraph grid(int n, boolean moore, boolean torus) {
+		SingleGraph myGraph = new SingleGraph("grid von Neumann or Moore");
+		// creation of nodes
+		for (int line = 0; line < n; line++) {
+			for (int col = 0; col < n; col++) {
+				Node v = myGraph.addNode(line + "-" + col);
+				v.addAttribute("x", col);
+				v.addAttribute("y", line);
+			}
 		}
+		// creation of edges for von Neumann neighborhood
+		int val = 1;
+		if (torus)
+			val = 0;
+		for (int line = 0; line < n; line++) {
+			for (int col = 0; col < n - val; col++) {
+				int colplusone = (col + 1) % n;
+				Node v = myGraph.getNode(line + "-" + col);
+				Node w = myGraph.getNode(line + "-" + colplusone);
+				Edge e = myGraph.addEdge(v.getId() + "_" + w.getId(), v.getId(), w.getId());
+			}
+		}
+
+		for (int col = 0; col < n; col++) {
+			for (int line = 0; line < n - val; line++) {
+				int lineplusone = (line + 1) % n;
+				Node v = myGraph.getNode(line + "-" + col);
+				Node w = myGraph.getNode(lineplusone + "-" + col);
+				Edge e = myGraph.addEdge(v.getId() + "_" + w.getId(), v.getId(), w.getId());
+			}
+		}
+		if (moore) {
+			// creation of additional edges corresponding to Moore neighborhood
+			// addition of north-east, north-west, south-east and south-west neighbors
+			for (int line = 0; line < n - val; line++) {
+				for (int col = 0; col < n - val; col++) {
+					int colplusone = (col + 1) % n;
+					int lineplusone = (line + 1) % n;
+					Node v = myGraph.getNode(line + "-" + col);
+					Node w = myGraph.getNode(lineplusone + "-" + colplusone);
+					Edge e = myGraph.addEdge(v.getId() + "_" + w.getId(), v.getId(), w.getId());
+				}
+			}
+			for (int line = val; line < n; line++) {
+				for (int col = 0; col < n - val; col++) {
+					int colplusone = (col + 1) % n;
+					int lineminusone = (n + line - 1) % n;
+					Node v = myGraph.getNode(line + "-" + col);
+					Node w = myGraph.getNode(lineminusone + "-" + colplusone);
+					Edge e = myGraph.addEdge(v.getId() + "_" + w.getId(), v.getId(), w.getId());
+				}
+			}
+		}
+		return myGraph;
 	}
-	
-	
+
+	/**
+	 * Full-connected (also known as complete) graphs generator
+	 * any vertex is connected to all the other vertices
+	 * 
+	 * @param n the number of nodes
+	 */
+
+	public static SingleGraph fullConnectedGraph(int n) {
+		SingleGraph myGraph = new SingleGraph("full-connected");
+		// creation of nodes
+		Node no = myGraph.addNode("n_0");
+		no.addAttribute("ui.style", "fill-color:red;size:10px;");
+		for (int i = 1; i < n; i++) {
+			myGraph.addNode("n_" + i);
+		}
+		// creation of edges
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = i + 1; j < n; j++) {
+				Edge e = myGraph.addEdge("E:" + i + "-" + j, "n_" + i, "n_" + j);
+				if (i == 0)
+					e.addAttribute("ui.style", "fill-color:red;size:3px;");
+			}
+		}
+		return myGraph;
+	}
+
+	/**
+	 * Erdos-Renyi model of random graphs
+	 * Nota: if p=1 then the graph is full-connected
+	 * 
+	 * @param n number of nodes
+	 * @param p probability of creation of any edge
+	 */
+	public static SingleGraph randomGraph(int n, double p) {
+		SingleGraph myGraph = new SingleGraph("erdos-renyi");
+		Random alea = new Random(System.currentTimeMillis());
+		// creation of vertices
+		for (int i = 0; i < n - 1; i++) {
+			myGraph.addNode("n_" + i);
+		}
+		// creation of edges according to the probability
+		for (int i = 0; i < n - 2; i++) {
+			for (int j = i + 1; j < n - 1; j++) {
+				if (alea.nextDouble() < p) {
+					myGraph.addEdge("E:" + i + "-" + j, "n_" + i, "n_" + j);
+				}
+			}
+		}
+		return myGraph;
+	}
+
+	// ============== VARIOUS TOOLS ================
+
+	/**
+	 * take a screenshot of the current graph
+	 * 
+	 * @param graph
+	 * @param pathToImage
+	 */
+	public static void screenshot(SingleGraph graph, String pathToImage) {
+		if (graph != null)
+			if (graph.getNodeCount() > 0) {
+				FileSinkImages fsi = new FileSinkImages(FileSinkImages.OutputType.PNG, FileSinkImages.Resolutions.SVGA);
+				fsi.setLayoutPolicy(FileSinkImages.LayoutPolicy.COMPUTED_FULLY_AT_NEW_IMAGE);
+				try {
+					fsi.writeAll(graph, pathToImage);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+	}
+
+	/**
+	 * generate a graph from a dgs file
+	 * 
+	 * @param pathtodgs
+	 * @return
+	 */
 	public static SingleGraph read(String pathtodgs) {
 		SingleGraph graph = new SingleGraph("");
 		try {
 			graph.read(pathtodgs);
-		} catch(Exception e) { }
+		} catch (Exception e) {
+		}
 		return graph;
 	}
-	
 
+	/**
+	 * pause the current execution
+	 * 
+	 * @param delay
+	 */
 	public static void pause(long delay) {
 		try {
 			Thread.sleep(delay);
-		} catch(InterruptedException ie) {}
+		} catch (InterruptedException ie) {
+		}
 	}
 
+	public static void hitakey(String msg) {
+		System.err.println("\n-----------");
+		System.err.println("\t" + msg);
+		System.err.println("-----------");
+		try {
+			System.in.read();
+		} catch (IOException ioe) {
+		}
+	}
 }
